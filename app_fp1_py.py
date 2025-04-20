45fec3fb4b274c225984cc8a4f34c3c03edfeb7d
ronitsalvi <101962712+ronitsalvi@users.noreply.github.com>
2025-04-20 20:18:11 +0530

    Update app_fp1_py.py

--- app_fp1_py.py
+++ app_fp1_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1_sAtq4xBlr_i_lSaRrzDMErF6VawGpfT
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import requests
