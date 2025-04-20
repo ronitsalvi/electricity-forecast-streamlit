29b294d6b429353be4a3993629ae08be366b0564
ronitsalvi <101962712+ronitsalvi@users.noreply.github.com>
2025-04-20 21:38:12 +0530

    Update app_fp1_py.py

--- app_fp1_py.py
+++ app_fp1_py.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 
-FASTAPI_URL = "https://1397-34-41-88-204.ngrok-free.app/predict"  # Replace
+FASTAPI_URL = "https://d039-34-41-88-204.ngrok-free.app/predict"  # Replace
 
 st.title("Energy Consumption Forecast")
 
@@ -18,10 +18,10 @@ if st.button("Predict"):
 
 
             df = pd.DataFrame({
-                # "Date": result["dates"],
-                "Forecast": result["forecast"]
-                # "Lower CI": result["lower"],
-                # "Upper CI": result["upper"],
+                "Date": result["dates"],
+                "Forecast": result["forecast"],
+                "Lower CI": result["lower"],
+                "Upper CI": result["upper"]
             })
 
             st.success("Forecast fetched!")
