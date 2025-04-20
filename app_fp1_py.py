099c1407caa1e737f498610df0553bd1a400191c
ronitsalvi <101962712+ronitsalvi@users.noreply.github.com>
2025-04-20 21:53:44 +0530

    Update app_fp1_py.py

--- app_fp1_py.py
+++ app_fp1_py.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
+import plotly.graph_objects as go
 
 FASTAPI_URL = "https://8f02-34-41-88-204.ngrok-free.app/predict"  # Replace
 
@@ -20,13 +21,22 @@ if st.button("Predict"):
             df = pd.DataFrame({
                 "Date": result["dates"],
                 "Forecast": result["forecast"],
-                "Lower CI": result["lower"],
-                "Upper CI": result["upper"]
+                "Lower_CI": result["lower"],
+                "Upper_CI": result["upper"]
             })
 
             st.success("Forecast fetched!")
             st.dataframe(df)
-            st.line_chart(df.set_index("Date")[["Forecast", "Lower CI", "Upper CI"]])
+            # Plot
+            # recent_actual = extended_series.dropna().iloc[-9:].round().astype(int)   
+            fig = go.Figure()    
+            # fig.add_trace(go.Scatter(x=recent_actual.index, y=recent_actual, mode='lines+markers', name='Last 12 Months Actual'))
+            fig.add_trace(go.Scatter(x=forecast.index, y=forecast, mode='lines+markers', name='Forecast'))
+            fig.add_trace(go.Scatter(x=forecast.index, y=Lower_CI, mode='lines', name='Lower CI', line=dict(dash='dot')))
+            fig.add_trace(go.Scatter(x=forecast.index, y=Upper_CI, mode='lines', name='Upper CI', line=dict(dash='dot')))
+            fig.update_layout(title="Forecast from Current Month Onward", xaxis_title="Date", yaxis_title="Consumption")
+            st.plotly_chart(fig)
+            # st.line_chart(df.set_index("Date")[["Forecast", "Lower CI", "Upper CI"]])
 
         except Exception as e:
             response = requests.get(FASTAPI_URL, params={"sector": sector, "months": months})
