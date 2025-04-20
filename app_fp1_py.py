0e53bc002a970bf7cbaba69a4ee2706fbc71f741
ronitsalvi <101962712+ronitsalvi@users.noreply.github.com>
2025-04-20 21:59:59 +0530

    Update app_fp1_py.py

--- app_fp1_py.py
+++ app_fp1_py.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import requests
-import plotly.graph_objects as go
 
 FASTAPI_URL = "https://8f02-34-41-88-204.ngrok-free.app/predict"  # Replace
 
@@ -28,15 +27,7 @@ if st.button("Predict"):
             st.success("Forecast fetched!")
             st.dataframe(df)
             # Plot
-            # recent_actual = extended_series.dropna().iloc[-9:].round().astype(int)   
-            fig = go.Figure()    
-            # fig.add_trace(go.Scatter(x=recent_actual.index, y=recent_actual, mode='lines+markers', name='Last 12 Months Actual'))
-            fig.add_trace(go.Scatter(x=forecast.index, y=forecast, mode='lines+markers', name='Forecast'))
-            fig.add_trace(go.Scatter(x=forecast.index, y=Lower_CI, mode='lines', name='Lower CI', line=dict(dash='dot')))
-            fig.add_trace(go.Scatter(x=forecast.index, y=Upper_CI, mode='lines', name='Upper CI', line=dict(dash='dot')))
-            fig.update_layout(title="Forecast from Current Month Onward", xaxis_title="Date", yaxis_title="Consumption")
-            st.plotly_chart(fig)
-            # st.line_chart(df.set_index("Date")[["Forecast", "Lower CI", "Upper CI"]])
+            st.line_chart(df.set_index("Date")[["Forecast", "Lower CI", "Upper CI"]])
 
         except Exception as e:
             response = requests.get(FASTAPI_URL, params={"sector": sector, "months": months})
