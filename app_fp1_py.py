582982834ea94526033c06f33492e0021a09b355
ronitsalvi <101962712+ronitsalvi@users.noreply.github.com>
2025-04-20 20:16:53 +0530

    Update app_fp1_py.py

--- app_fp1_py.py
+++ app_fp1_py.py
@@ -13,7 +13,7 @@ import pandas as pd
 import requests
 
 # Replace this with your live ngrok URL (copy from Colab after running ngrok)
-FASTAPI_URL = "https://3e05-34-136-243-126.ngrok-free.app"
+FASTAPI_URL = "https://9dfa-34-41-88-204.ngrok-free.app"
 
 st.title("⚡ Energy Consumption Forecast")
 
